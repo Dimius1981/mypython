0c0aad14c4d213909cb464266ede9c959d4f6d57
dmitriy.ponkratyev <pdm@mail.ru>
2022-05-11 14:01:30 +0600

    Переделал задачу 5 с использованием функций

--- 3_5.py
+++ 3_5.py
@@ -7,6 +7,20 @@
 # Но если вместо числа вводится специальный символ, выполнение программы завершается.
 # Если специальный символ введён после нескольких чисел, то вначале нужно добавить сумму
 # этих чисел к полученной ранее сумме и после этого завершить программу.
+def str2num(astr):
+    if astr.isnumeric():
+        return int(astr)
+    return 0
+
+
+def sumstrsplit(astr):
+    cur_sum = 0
+    for el in astr.split():
+        cur_sum += str2num(el)
+        if el == exit_sym:
+            return cur_sum, True  # return sum, exit_flag
+    return cur_sum, False  # return sum, exit_flag
+
 
 exit_sym = 'q'
 exit_flag = False
@@ -15,13 +29,8 @@ my_sum = 0
 
 while True:
     my_str = input("Введите строку чисел через пробел: ")
-    for el in my_str.split():
-        # print(el)
-        if el.replace("-", "").isnumeric():
-            my_sum += int(el)
-        if el == exit_sym:
-            exit_flag = True
-            break
+    str_sum, exit_flag = sumstrsplit(my_str)
+    my_sum += str_sum
     print(f"Сумма: {my_sum}")
     if exit_flag:
         break
